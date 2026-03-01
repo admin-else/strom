f7d7a72b7ca5ee4eea0ad5f32f49f17d89cf7cd9
adman <adman.else@gmail.com>
2026-03-01 13:28:49 +0100

    nbt files

--- nbt/file.go
+++ nbt/file.go
@@ -1,1 +1,27 @@
 package nbt
+
+import (
+	"compress/gzip"
+	"io"
+)
+
+func WriteFile(file io.Writer, n Tag) (err error) {
+	gzw := gzip.NewWriter(file)
+	if err != nil {
+		return
+	}
+	defer gzw.Close()
+	err = n.Encode(gzw)
+	return
+}
+
+func ReadFile(file io.Reader) (err error, n Tag) {
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return
+	}
+	defer r.Close()
+	t := &Tag{}
+	err = t.Decode(r)
+	return
+}
