66f32b4cd2af4310f29a81d2dab3b117f51dca55
adman <adman.else@gmail.com>
2026-05-03 14:39:31 +0200

    update minecraft data binding to make binary smaller

--- data/data.go
+++ data/data.go
@@ -4,7 +4,10 @@ import (
 	"embed"
 	"encoding/json"
 	"errors"
+	"fmt"
 )
+
+//go:generate ./download_minecraft_data.sh
 
 //go:embed minecraft-data/pc minecraft-data/dataPaths.json
 var MinecraftData embed.FS
@@ -21,7 +24,8 @@ func LoadJson(path string, data any) (err error) {
 func LoadVersionedJson(version, dataName string, data any) (err error) {
 	b, err := MinecraftData.ReadFile("minecraft-data/" + Paths.Data[version][dataName] + "/" + dataName + ".json")
 	if err != nil {
-		return
+		// If you get this error, you need to probably need to update the download script
+		return fmt.Errorf("minecraft-data/%s/%s/%s.json: %w", version, Paths.Data[version][dataName], dataName, err)
 	}
 	err = json.Unmarshal(b, data)
 	return
--- data/download_minecraft_data.sh
+++ data/download_minecraft_data.sh
@@ -1,10 +1,25 @@
-#!/usr/bin/env sh
+#!/usr/bin/sh
+set -xeuo pipefail
 
 # Go get / install does not clone git submodules so we have to do it like this
 
 rm -rf minecraft-data/
 git clone --depth 1 git@github.com:PrismarineJS/minecraft-data.git
-mv minecraft-data/data data
-rm -rf minecraft-data
-mv data minecraft-data
-rm -rf minecraft-data/bedrock
+mv minecraft-data minecraft-data-untrimmed
+
+mkdir minecraft-data
+mkdir minecraft-data/pc
+
+mv minecraft-data-untrimmed/data/dataPaths.json minecraft-data
+
+copy-mc() {
+  mv minecraft-data-untrimmed/data/pc/$1 minecraft-data/pc
+}
+
+copy-mc common
+
+copy-mc 1.21.8
+copy-mc 1.21.9
+copy-mc 1.21.11
+
+rm -rf minecraft-data-untrimmed
